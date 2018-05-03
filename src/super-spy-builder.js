f6bb83d12dbe5ad10ead9759ffb7bdaf196a7177
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2018-05-03 18:42:06 -0400

    Refactor super spy into a builder that allows mocking of inherited methods

---
+++ src/super-spy-builder.js
@@ -0,0 +1,189 @@
+'use strict';
+
+const _sinon = require('sinon');
+
+/**
+ * Builder class that can be used to inject an intermediate spy between a parent
+ * class and child class, making it possible to spy on <strong>super()</strong>
+ * calls made by the child.
+ *
+ * The builder also supports injection of mocks on the parent class' prototype,
+ * enabling spying on calls made by the child class on methods inherited from
+ * the parent class.
+ *
+ * The class exposes methods that will allow the mock/spy references to be torn
+ * down after testing.
+ *
+ * Calls to <strong>super()</strong> will automatically be spied upon, and the
+ * parent class' <strong>super()</strong> method will be called after the spy
+ * has been called.
+ */
+class SuperSpyBuilder {
+    /**
+     * @param {Class} ParentClass The parent class
+     * @param {Class} ChildClass The child class
+     */
+    constructor(ParentClass, ChildClass) {
+        if (typeof ParentClass !== 'function') {
+            throw new Error('Invalid Parent class (arg #1)');
+        }
+        if (typeof ChildClass !== 'function') {
+            throw new Error('Invalid Child class (arg #1)');
+        }
+
+        this._spiedMethods = {};
+        this._mockDefinitions = {};
+        this._mocks = {
+            super: {
+                stub: _sinon.spy()
+            }
+        };
+        this._ParentClass = ParentClass;
+        this._ChildClass = ChildClass;
+    }
+
+    /**
+     * An object containing references to stub instances for each of the methods
+     * on the parent that has been mocked. The stubs returned are references to
+     * [sinon stubs]{@link http://sinonjs.org/releases/v4.5.0/stubs/}.
+     *
+     * @type {Object}
+     */
+    get mocks() {
+        return this._mocks;
+    }
+
+    /**
+     * Creates a mock on the parent class. The mock method intercepts any calls
+     * to the parent method, and logs the invocation against the corresponding
+     * mock spy reference.
+     *
+     * <p>The mocks are not applied to the parent class' prototype until the
+     * {@link SuperSpyBuilder.inject} method is called. </p>
+     *
+     * @param {String} method The name of the method on the parent on which to
+     *        apply the mock. If a value of "super()" is passed, an error will
+     *        be thrown. This method must be defined by the parent class.
+     * @param {Function} [fake=undefined] A fake function that will be invoked
+     *        every time the parent method is called. If possible, the function
+     *        will be invoked in the context of the instance with a valid
+     *        <strong>this</strong> parameter. This will not work if the fake is
+     *        an
+     *        [arrow function]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions}
+     * @param {Boolean} [skipSuperMethod=false] A boolean that indicates if the
+     *        actual method on the parent should be invoked after the fake is
+     *        invoked. The parent method will be invoked by default.
+     *
+     * @return {Object} A reference to this object (can be used to chain method
+     *         calls)
+     */
+    addMock(method, fake, skipSuperMethod) {
+        if (typeof method !== 'string' || method.length <= 0) {
+            throw new Error('Invalid method (arg #1)');
+        }
+        if (method === 'super') {
+            throw new Error('Method name cannot be "super" (arg #1)');
+        }
+
+        if (typeof this._ParentClass.prototype[method] !== 'function') {
+            throw new Error(`Parent class does not define method: [${method}]`);
+        }
+        if (typeof fake === 'undefined') {
+            fake = () => {};
+        }
+        if (typeof fake !== 'function') {
+            throw new Error('Invalid fake (arg #2)');
+        }
+
+        this._mockDefinitions[method] = {
+            fake,
+            invokeSuperMethod: !skipSuperMethod
+        };
+    }
+
+    /**
+     * Generates and injects a spy class between a parent and child class,
+     * changing the inheritance hierarchy from:
+     *
+     * <p><strong>ParentClass --> Child hierarchy</strong></p>
+     *
+     * to:
+     *
+     * <p><strong>ParentClass --> SuperSpy --> Child</strong></p>
+     *
+     * <p>
+     * Additionally, this method modifies the parent class prototype, creating
+     * mocks as defined by calls to [addMock()]{@link SuperSpyBuilder#addMock}.
+     * </p>
+     *
+     * @return {SuperSpy} A reference to the spy class, that is injected between
+     *         the parent and the child.
+     */
+    inject() {
+        const mocks = this.mocks;
+
+        /**
+         * Intermediate class that is generated and injected into the hierarchy
+         * between the parent class and the child class.  This class will mirror
+         * all constructor to a
+         * [sinon stub]{@link http://sinonjs.org/releases/v4.5.0/stubs/} that
+         * can be inspected to perform tests on constructor calls. This allows
+         * calls to super() to be tested.
+         *
+         * <p>
+         * This class is not meant to be (and cannot be) instantiated directly.
+         * </p>
+         */
+        class SuperSpy extends this._ParentClass {
+            constructor(...args) {
+                mocks.super.stub(...args);
+                super(...args);
+            }
+        }
+
+        Object.setPrototypeOf(this._ChildClass, SuperSpy);
+
+        Object.keys(this._mockDefinitions).forEach((method) => {
+            const { fake, invokeSuperMethod } = this._mockDefinitions[method];
+            const originalMethod = this._ParentClass.prototype[method];
+            const stub = _sinon
+                .stub(this._ParentClass.prototype, method)
+                .callsFake(function(...args) {
+                    let ret = fake.call(this, ...args);
+                    if (invokeSuperMethod) {
+                        ret = originalMethod.call(this, ...args);
+                    }
+                    return ret;
+                });
+
+            this.mocks[method] = {
+                stub
+            };
+        });
+
+        return SuperSpy;
+    }
+
+    /**
+     * Restores the relationship between parent and child, by removing the spy
+     * class from the hierarchy, changing the inheritance hierarchy from:
+     * <p><strong>ParentClass --> SuperSpy --> Child</strong></p>
+     * to:
+     * <p><strong>ParentClass --> Child</strong></p>
+     * <p>
+     * Any mocks injected into the parent class' prototype are also removed,
+     * restoring the Parent prototype to its original form.
+     * </p>
+     */
+    restore() {
+        Object.setPrototypeOf(this._ChildClass, this._ParentClass);
+        Object.keys(this._mockDefinitions).forEach((method) => {
+            const methodRef = this._ParentClass.prototype[method];
+            if (typeof methodRef.restore === 'function') {
+                methodRef.restore();
+            }
+        });
+    }
+}
+
+module.exports = SuperSpyBuilder;
---
+++ test/unit/super-spy-builder-spec.js
@@ -0,0 +1,518 @@
+'use strict';
+
+const _chai = require('chai');
+_chai.use(require('sinon-chai'));
+_chai.use(require('chai-as-promised'));
+const expect = _chai.expect;
+
+const _shortId = require('shortid');
+const _sinon = require('sinon');
+const _rewire = require('rewire');
+
+const SuperSpyBuilder = _rewire('../../src/super-spy-builder');
+
+describe('SuperSpyBuilder', () => {
+    function _getParentAndChildClasses() {
+        class Parent {
+            testMethod1() {}
+            testMethod2() {}
+            get testProp1() {
+                return '';
+            }
+        }
+
+        class Child extends Parent {
+            constructor(...args) {
+                super(...args);
+            }
+        }
+
+        return { Parent, Child };
+    }
+
+    function _createInstance(Parent, Child) {
+        const defaultClasses = _getParentAndChildClasses();
+        Parent = Parent || defaultClasses.Parent;
+        Child = Child || defaultClasses.Child;
+
+        return new SuperSpyBuilder(Parent, Child);
+    }
+
+    function _getArgs() {
+        return [undefined, null, 'foo', true, {}, [], 123, () => {}];
+    }
+
+    describe('ctor()', () => {
+        it('should throw an error if invoked without a valid parent class', () => {
+            const error = 'Invalid Parent class (arg #1)';
+            const inputs = [undefined, null, 'foo', true, {}, [], 123, -1];
+
+            inputs.forEach((Parent) => {
+                const wrapper = () => {
+                    return new SuperSpyBuilder(Parent);
+                };
+
+                expect(wrapper).to.throw(error);
+            });
+        });
+
+        it('should throw an error if invoked without a valid child class', () => {
+            const error = 'Invalid Child class (arg #1)';
+            const inputs = [undefined, null, 'foo', true, {}, [], 123, -1];
+
+            inputs.forEach((Child) => {
+                const wrapper = () => {
+                    const Parent = () => {};
+                    return new SuperSpyBuilder(Parent, Child);
+                };
+
+                expect(wrapper).to.throw(error);
+            });
+        });
+
+        it('should return an object with the expected methods and properties', () => {
+            const { Parent, Child } = _getParentAndChildClasses();
+            const builder = new SuperSpyBuilder(Parent, Child);
+
+            expect(builder).to.be.an('object');
+            expect(builder.mocks).to.be.an('object');
+            expect(builder.inject).to.be.a('function');
+            expect(builder.addMock).to.be.a('function');
+            expect(builder.restore).to.be.a('function');
+        });
+    });
+
+    describe('mocks', () => {
+        it('should contain a spy for the super() call', () => {
+            const builder = _createInstance();
+            const mocks = builder.mocks;
+
+            expect(mocks).to.be.an('object');
+            expect(Object.keys(mocks)).to.deep.equal(['super']);
+            expect(mocks.super).to.be.an('object');
+            expect(mocks.super.stub).to.be.a('function');
+        });
+    });
+
+    describe('inject()', () => {
+        it('should return a SpyClass that extends the parent', () => {
+            const { Parent, Child } = _getParentAndChildClasses();
+            const builder = _createInstance(Parent, Child);
+            const SpyClass = builder.inject();
+
+            const spy = new SpyClass();
+            expect(spy).to.be.an.instanceof(Parent);
+        });
+
+        it('should modify the prototype of the child class to point to SpyClass', () => {
+            const { Parent, Child } = _getParentAndChildClasses();
+            const builder = _createInstance(Parent, Child);
+            const SpyClass = builder.inject();
+
+            expect(Object.getPrototypeOf(Child)).to.equal(SpyClass);
+        });
+
+        it('should invoke the super spy with a copy of all arguments passed to it', () => {
+            const { Child } = _getParentAndChildClasses();
+            const builder = _createInstance(undefined, Child);
+
+            builder.inject();
+
+            const args = _getArgs();
+            const superMethod = builder.mocks.super;
+
+            expect(superMethod.stub).to.not.have.been.called;
+
+            new Child(...args);
+
+            expect(superMethod.stub).to.have.been.calledOnce;
+            args.forEach((arg, index) => {
+                expect(superMethod.stub.args[0][index]).to.deep.equal(arg);
+            });
+        });
+
+        it('should invoke super() of the parent with all arguments passed to it', () => {
+            const ctorSpy = _sinon.spy();
+            class Parent {
+                constructor(...args) {
+                    ctorSpy(...args);
+                }
+            }
+            class Child extends Parent {}
+
+            const builder = _createInstance(Parent, Child);
+            const SpyClass = builder.inject();
+            const args = _getArgs();
+
+            expect(ctorSpy).to.not.have.been.called;
+
+            new SpyClass(...args);
+
+            expect(ctorSpy).to.have.been.calledOnce;
+            args.forEach((arg, index) => {
+                expect(ctorSpy.args[0][index]).to.deep.equal(arg);
+            });
+        });
+    });
+
+    describe('addMock()', () => {
+        it('should throw an error if invoked without a valid method', () => {
+            const error = 'Invalid method (arg #1)';
+            const inputs = [undefined, null, '', true, {}, [], 123, () => {}];
+
+            inputs.forEach((method) => {
+                const wrapper = () => {
+                    const builder = _createInstance();
+                    builder.addMock(method);
+                };
+
+                expect(wrapper).to.throw(error);
+            });
+        });
+
+        it('should throw an error if the method name is "super()"', () => {
+            const error = 'Method name cannot be "super" (arg #1)';
+
+            const wrapper = () => {
+                const builder = _createInstance();
+                builder.addMock('super');
+            };
+            expect(wrapper).to.throw(error);
+        });
+
+        it('should throw an error if the spy methods are not valid functions on the parent', () => {
+            const inputs = ['foo', 'bar', 'testProp1'];
+
+            inputs.forEach((method) => {
+                const error = `Parent class does not define method: [${method}]`;
+
+                const wrapper = () => {
+                    const builder = _createInstance();
+                    builder.addMock(method);
+                };
+
+                expect(wrapper).to.throw(error);
+            });
+        });
+
+        it('should throw an error if a fake is defined and not a valid function', () => {
+            const error = 'Invalid fake (arg #2)';
+            const inputs = [null, 'foo', true, {}, [], 123];
+
+            inputs.forEach((fake) => {
+                const wrapper = () => {
+                    const builder = _createInstance();
+                    builder.addMock('testMethod1', fake);
+                };
+
+                expect(wrapper).to.throw(error);
+            });
+        });
+
+        it('should not throw an error if the fake is undefined', () => {
+            const wrapper = () => {
+                const builder = _createInstance();
+                builder.addMock('testMethod1', undefined);
+            };
+
+            expect(wrapper).to.not.throw();
+        });
+
+        it('should create a spy for the spy method name', () => {
+            const builder = _createInstance();
+            const methods = ['testMethod1', 'testMethod2'];
+
+            methods.forEach((method) => {
+                builder.addMock(method);
+            });
+
+            builder.inject();
+
+            const methodNames = Object.keys(builder.mocks);
+            expect(methodNames).to.have.members(methods.concat('super'));
+        });
+
+        it('should invoke method spy with all arguments passed to it', () => {
+            const builder = _createInstance();
+            const methods = ['testMethod1', 'testMethod2'];
+            methods.forEach((method) => {
+                builder.addMock(method);
+            });
+
+            const SpyClass = builder.inject();
+            const spy = new SpyClass();
+
+            const args = _getArgs();
+            methods.forEach((method) => {
+                const methodRef = builder.mocks[method];
+                expect(methodRef.stub).to.not.have.been.called;
+                spy[method](...args);
+                expect(methodRef.stub).to.have.been.calledOnce;
+                args.forEach((arg, index) => {
+                    expect(methodRef.stub.args[0][index]).to.deep.equal(arg);
+                });
+            });
+        });
+
+        it('should invoke the fake if a fake was specified with the mock', () => {
+            const builder = _createInstance();
+            const methods = ['testMethod1', 'testMethod2'];
+            const fakes = {};
+
+            methods.forEach((method) => {
+                const fake = _sinon.spy();
+                builder.addMock(method, fake);
+                fakes[method] = fake;
+            });
+
+            const SpyClass = builder.inject();
+            const spy = new SpyClass();
+
+            const args = _getArgs();
+            methods.forEach((method) => {
+                const fake = fakes[method];
+                expect(fake).to.not.have.been.called;
+                spy[method](...args);
+                expect(fake).to.have.been.calledOnce;
+                args.forEach((arg, index) => {
+                    expect(fake.args[0][index]).to.deep.equal(arg);
+                });
+            });
+        });
+
+        it('should invoke the fake with the correct "this" binding', () => {
+            const builder = _createInstance();
+            const methods = ['testMethod1', 'testMethod2'];
+            const results = [_shortId.generate(), _shortId.generate()];
+
+            methods.forEach((method, index) => {
+                const fake = function() {
+                    this.newProperty = results[index];
+                };
+                builder.addMock(method, fake);
+            });
+
+            const SpyClass = builder.inject();
+
+            methods.forEach((method, index) => {
+                const spy = new SpyClass();
+                expect(spy.newProperty).to.be.undefined;
+                spy[method]();
+                expect(spy.newProperty).to.equal(results[index]);
+            });
+        });
+
+        it('should not invoke the super class method if skipSuperMethod is truthy', () => {
+            const flags = [1, 'foo', {}, [], () => {}];
+            flags.forEach((flag) => {
+                const { Parent } = _getParentAndChildClasses();
+                const parentMethodSpies = {};
+                const methods = ['foo', 'bar', 'baz'];
+                methods.forEach((method) => {
+                    const spy = _sinon.spy();
+                    parentMethodSpies[method] = spy;
+                    Parent.prototype[method] = function(...args) {
+                        spy(...args);
+                    };
+                });
+                const builder = _createInstance(Parent);
+
+                methods.forEach((method) => {
+                    builder.addMock(method, undefined, flag);
+                });
+
+                const SpyClass = builder.inject();
+                const spy = new SpyClass();
+
+                const args = _getArgs();
+                methods.forEach((method) => {
+                    const parentMethod = parentMethodSpies[method];
+
+                    expect(parentMethod).to.not.have.been.called;
+                    spy[method](...args);
+                    expect(parentMethod).to.not.have.been.called;
+
+                    parentMethod.resetHistory();
+                });
+            });
+        });
+
+        it('should return the response from the fake if skipSuperMethod is truthy', () => {
+            const flags = [1, 'foo', {}, [], () => {}];
+            flags.forEach((flag) => {
+                const builder = _createInstance();
+                const methods = ['testMethod1', 'testMethod2'];
+                const results = [_shortId.generate(), _shortId.generate()];
+
+                methods.forEach((method, index) => {
+                    const fake = function() {
+                        return results[index];
+                    };
+                    builder.addMock(method, fake, flag);
+                });
+
+                const SpyClass = builder.inject();
+
+                methods.forEach((method, index) => {
+                    const spy = new SpyClass();
+                    const ret = spy[method]();
+                    expect(ret).to.equal(results[index]);
+                });
+            });
+        });
+
+        it('should invoke the super class method if skipSuperMethod is falsy', () => {
+            const flags = [undefined, 0, '', null, false];
+            flags.forEach((flag) => {
+                const { Parent } = _getParentAndChildClasses();
+                const parentMethodSpies = {};
+                const methods = ['foo', 'bar', 'baz'];
+                methods.forEach((method) => {
+                    const spy = _sinon.spy();
+                    parentMethodSpies[method] = spy;
+                    Parent.prototype[method] = function(...args) {
+                        spy(...args);
+                    };
+                });
+                const builder = _createInstance(Parent);
+
+                const SpyClass = builder.inject();
+                const spy = new SpyClass();
+
+                methods.forEach((method) => {
+                    builder.addMock(method, undefined, flag);
+                });
+
+                const args = _getArgs();
+                methods.forEach((method) => {
+                    const parentMethod = parentMethodSpies[method];
+
+                    expect(parentMethod).to.not.have.been.called;
+                    spy[method](...args);
+                    expect(parentMethod).to.have.been.calledOnce;
+                    args.forEach((arg, index) => {
+                        expect(parentMethod.args[0][index]).to.deep.equal(arg);
+                    });
+
+                    parentMethod.resetHistory();
+                });
+            });
+        });
+
+        it('should return the response from the super class method if skipSuperMethod is falsy', () => {
+            const flags = [undefined, 0, '', null, false];
+            flags.forEach((flag) => {
+                const { Parent } = _getParentAndChildClasses();
+                const methods = ['foo', 'bar'];
+                const results = [_shortId.generate(), _shortId.generate()];
+                methods.forEach((method, index) => {
+                    Parent.prototype[method] = function() {
+                        return results[index];
+                    };
+                });
+                const builder = _createInstance(Parent);
+
+                methods.forEach((method, index) => {
+                    builder.addMock(method, undefined, flag);
+                });
+
+                const SpyClass = builder.inject();
+
+                methods.forEach((method, index) => {
+                    const spy = new SpyClass();
+                    const ret = spy[method]();
+                    expect(ret).to.equal(results[index]);
+                });
+            });
+        });
+
+        it('should invoke the super class method with the correct "this" binding', () => {
+            const flags = [undefined, 0, '', null, false];
+            flags.forEach((flag) => {
+                const { Parent } = _getParentAndChildClasses();
+                const methods = ['foo', 'bar'];
+                const results = [_shortId.generate(), _shortId.generate()];
+                methods.forEach((method, index) => {
+                    Parent.prototype[method] = function() {
+                        this.newProperty = results[index];
+                    };
+                });
+                const builder = _createInstance(Parent);
+
+                methods.forEach((method, index) => {
+                    builder.addMock(method, undefined, flag);
+                });
+
+                const SpyClass = builder.inject();
+
+                methods.forEach((method, index) => {
+                    const spy = new SpyClass();
+                    expect(spy.newProperty).to.be.undefined;
+                    spy[method]();
+                    expect(spy.newProperty).to.equal(results[index]);
+                });
+            });
+        });
+    });
+
+    describe('restore()', () => {
+        it('should remove the spy class from the class hierarchy', () => {
+            const { Parent, Child } = _getParentAndChildClasses();
+            const builder = _createInstance(Parent, Child);
+
+            builder.inject();
+            builder.restore();
+
+            expect(Object.getPrototypeOf(Child)).to.equal(Parent);
+        });
+
+        it('should remove any mocks created by the builder', () => {
+            const { Parent } = _getParentAndChildClasses();
+            const builder = _createInstance(Parent);
+            const methods = ['testMethod1', 'testMethod2'];
+            const originalMethods = {};
+
+            methods.forEach((method) => {
+                originalMethods[method] = Parent.prototype[method];
+            });
+
+            methods.forEach((method) => {
+                builder.addMock(method, undefined);
+            });
+
+            builder.inject();
+
+            methods.forEach((method) => {
+                const parentMethod = Parent.prototype[method];
+                const originalMethod = originalMethods[method];
+
+                expect(parentMethod).to.not.equal(originalMethod);
+            });
+
+            builder.restore();
+
+            methods.forEach((method) => {
+                const parentMethod = Parent.prototype[method];
+                const originalMethod = originalMethods[method];
+
+                expect(parentMethod).to.equal(originalMethod);
+            });
+        });
+
+        it('should do nothing if the spy has not been injected', () => {
+            const { Parent } = _getParentAndChildClasses();
+            const builder = _createInstance(Parent);
+            const methods = ['testMethod1', 'testMethod2'];
+
+            methods.forEach((method) => {
+                builder.addMock(method, undefined);
+            });
+
+            const wrapper = () => {
+                builder.restore();
+            };
+
+            expect(wrapper).to.not.throw();
+        });
+    });
+});
