f738807dfa097fa15e28bef2c1cc49b793f19868
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2022-01-22 14:50:07 -0500

    Convert source files to typescript

---
+++ src/async-helper.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Exposes utility methods to help execution of async tests.
+ * @module asyncHelper
+ */
+
+/**
+ * Returns a function that can be used to trigger an asynchronous delay. When
+ * invoked, the function returns a promise that will be resolved after the
+ * specified dealay. The delay function accepts data that will be passed as the
+ * result of the resolution of the promise.
+ *
+ * Useful when relying on timing to ensure that tests are evaluated correctly,
+ * or to yield to the javascript runtime.
+ *
+ * @param delay The delay (in milliseconds) after which the promise will be
+ * resolved.
+ *
+ * @return A function that returns a romise that will be resolved once the delay
+ *         expires.
+ */
+export function wait<T>(delay: number): (data: T) => Promise<T> {
+    if (typeof delay !== 'number' || delay <= 0) {
+        throw new Error('Invalid delay specified (arg #1)');
+    }
+    return (data: T) => {
+        return new Promise((resolve) => setTimeout(() => resolve(data), delay));
+    };
+}
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * Utility library that provides useful functionality for writing tests.
+ */
+
+/**
+ * Helper module that provides utilities for testing of async methods.
+ */
+export * as asyncHelper from './async-helper';
+
+// module.exports = {
+
+//     /**
+//      * Helper module that provides utilities for manipulating the behavior of
+//      * console statements.
+//      */
+//     consoleHelper: require('./console-helper'),
+
+//     /**
+//      * Helper module that generates random values to be used during testing.
+//      */
+//     testValues: require('./test-values'),
+
+//     /**
+//      * Class that can be used to create spies on parent methods in class
+//      * hierarchies.
+//      */
+//     SuperSpyBuilder: require('./super-spy-builder'),
+
+//     /**
+//      * Class that can be used to mock out methods on existing objects, or
+//      * create new objects with mocked methods.
+//      */
+//     ObjectMock: require('./object-mock'),
+// };
