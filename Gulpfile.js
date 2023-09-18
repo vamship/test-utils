f63c63a41d16476b5539792083623ff6fb58a0f0
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-09-18 10:57:47 -0400

    Upgraded package to ESM, reference alpha release of build-utils

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,51 +1,19 @@
-'use strict';
-
 /**
  * Dynamically defines develop/test/build/deploy tasks for the current project
  * based on metadata present in package.json.
  */
-const _log = require('fancy-log');
-const _colors = require('ansi-colors');
+import _log from 'fancy-log';
+import _colors from 'ansi-colors';
+import _fs from 'fs';
+import _gulp from 'gulp';
+import { Project, LibTaskFactory } from '@vamship/build-utils';
 
-const { Project, taskBuilders } = require('@vamship/build-utils');
-
-const project = new Project(require('./package.json'));
-
+const _package = JSON.parse(_fs.readFileSync('./package.json', 'utf-8'));
+const project = new Project(_package);
 const projectInfo = `${_colors.cyan.bold(project.name)} (${_colors.blue(
-    project.projectType
-)}, ${_colors.green(project.language)})`;
+    project.type
+)} - ${_colors.green(project.language)})`;
 
 _log.info(`Initializing tasks for project: ${projectInfo}`);
-
-const builders = [
-    { build: taskBuilders.clean },
-    { build: taskBuilders.format },
-    { build: taskBuilders.lint },
-    { build: taskBuilders.build },
-    { build: taskBuilders.docs },
-    { build: taskBuilders.package },
-    { build: taskBuilders.package, options: { types: true } },
-    { build: taskBuilders.publish },
-    { build: taskBuilders.publish, options: { latestOnly: true } },
-    { build: taskBuilders.publish, options: { types: true } },
-    { build: taskBuilders.package, options: { types: true, latestOnly: true } },
-
-    { build: taskBuilders.test, options: { testType: 'unit' } },
-    { build: taskBuilders.test, options: { testType: 'api' } },
-    { build: taskBuilders.format, options: { watch: true } },
-    { build: taskBuilders.lint, options: { watch: true } },
-    { build: taskBuilders.build, options: { watch: true } },
-    { build: taskBuilders.test, options: { testType: 'unit', watch: true } },
-    { build: taskBuilders.test, options: { testType: 'api', watch: true } },
-];
-
-const tasks = builders
-    .map(({ build, options }) => build(project, Object.assign({}, options)))
-    .reduce((result, tasks) => result.concat(tasks), [])
-    .filter((task) => !!task)
-    .reduce((result, task) => {
-        result[task.displayName] = task;
-        return result;
-    }, {});
-
-module.exports = tasks;
+const factory = new LibTaskFactory(project);
+factory.createTasks().forEach((task) => _gulp.task(task));
