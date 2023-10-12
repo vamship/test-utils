6e950c140bfadf40df07eaa1c46641513b939994
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2023-10-12 14:38:58 -0400

    Use exported function to dynamically configure build tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,7 @@ import _log from 'fancy-log';
 import _colors from 'ansi-colors';
 import _fs from 'fs';
 import _gulp from 'gulp';
-import { Project, LibTaskFactory } from '@vamship/build-utils';
+import { Project, getTaskFactory } from '@vamship/build-utils';
 
 const _package = JSON.parse(_fs.readFileSync('./package.json', 'utf-8'));
 const project = new Project(_package);
@@ -15,5 +15,5 @@ const projectInfo = `${_colors.cyan.bold(project.name)} (${_colors.blue(
 )} - ${_colors.green(project.language)})`;
 
 _log.info(`Initializing tasks for project: ${projectInfo}`);
-const factory = new LibTaskFactory(project);
+const factory = getTaskFactory(project);
 factory.createTasks().forEach((task) => _gulp.task(task));
