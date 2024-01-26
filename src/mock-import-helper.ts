9a589e13d5952546692d83fc44ffa56fd92c6de2
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2024-01-26 12:07:27 -0500

    Implement mock import helper

---
+++ src/mock-import-helper.ts
@@ -0,0 +1,202 @@
+import _path from 'path';
+
+/**
+ * Declarations of dependencies that need to be mocked. These are specified by
+ * mapping a string `mock key` to a dependency `import path`.
+ *
+ * @remarks
+ * The `mock key` can be any string, and is used to specify mocks when importing
+ * the target module.
+ *
+ * The `import path` is the path to the dependency that needs to be mocked. It
+ * is a non-empty string that supports special prefixes:
+ *
+ * - `project://` The path is assumed to be relative to the source root of the
+ * project, and can be used to mock modules defined within the project.
+ * - `global://` The path is assumed to be a node js global (such as console).
+ * - All other non prefixed paths are treated as external modules from
+ * node_modules.
+ */
+export type MockDeclarations = Record<string, string>;
+
+/**
+ * Definitions of mocks that need to be injected into the module. This is a map
+ * that maps a `mock key` to a mock definition, which is a mock object that
+ * replaces the module that would be imported.
+ */
+/* eslint-disable  tsel/no-explicit-any */
+export type MockDefinitions = Record<string, any>;
+
+// Private type
+type MockDefinitionMap = {
+    mockKey: string;
+    importPath: string;
+    mockDefinition: any;
+    isSrc: boolean;
+    isGlobal: boolean;
+};
+
+/**
+ * Class that can be used to generate mock definitions for use with esmock to
+ * import modules with injected mocks.
+ *
+ * Supports generation of mock definitions of local files, libraries installed
+ * in node_modules, and also globals (such as console).
+ *
+ * @typeParam T The type of the module that is being imported.
+ */
+export class MockImportHelper<T> {
+    private readonly _importPath: string;
+    private readonly _mockDeclarations: MockDeclarations;
+    private readonly _srcRoot: string;
+    private readonly _mockDefinitions: MockDefinitions = {};
+
+    /**
+     * Initializes a helper object for the specified module.
+     *
+     * @param importPath The path to the module to import.
+     * @param mockDeclarations A map of mock declarations that identify the
+     * dependencies that need to be mocked. Each declaration is a mapping of a
+     * mock key to an import path. The mock key can be used to set mocks prior
+     * to generating the mock definitions.
+     * @param srcRoot The path to the source root of the project. This is used
+     * to resolve local modules.
+     */
+    public constructor(
+        importPath: string,
+        mockDeclarations: MockDeclarations,
+        srcRoot: string,
+    ) {
+        if (typeof importPath !== 'string' || importPath.length <= 0) {
+            throw new Error('Invalid importPath (arg #1)');
+        }
+
+        if (
+            !mockDeclarations ||
+            mockDeclarations instanceof Array ||
+            typeof mockDeclarations !== 'object'
+        ) {
+            throw new Error('Invalid mockDeclarations (arg #2)');
+        }
+
+        if (typeof srcRoot !== 'string' || srcRoot.length <= 0) {
+            throw new Error('Invalid srcRoot (arg #3)');
+        }
+
+        this._importPath = importPath;
+        this._mockDeclarations = mockDeclarations;
+        this._srcRoot = srcRoot;
+    }
+
+    private _getActualPath(path: string): string {
+        if (path.startsWith('global://')) {
+            return path.replace(/^global:\/\//, '');
+        } else if (path.startsWith('project://') || path.startsWith('.')) {
+            return _path.resolve(
+                this._srcRoot,
+                path.replace(/^project:\/\//, ''),
+            );
+        } else {
+            return path;
+        }
+    }
+
+    private _getMockDefinitions(): MockDefinitionMap[] {
+        return Object.keys(this._mockDefinitions).map(
+            (mockKey: string): MockDefinitionMap => {
+                const mockDefinition = this._mockDefinitions[mockKey];
+                const importPath = this._mockDeclarations[mockKey];
+                return {
+                    mockKey,
+                    importPath: this._getActualPath(importPath),
+                    mockDefinition,
+                    isSrc: importPath.startsWith('project://'),
+                    isGlobal: importPath.startsWith('global://'),
+                };
+            },
+        );
+    }
+
+    /**
+     * Gets the import path for the module.
+     */
+    public get importPath(): string {
+        return this._getActualPath(this._importPath);
+    }
+
+    /**
+     * Sets a mock for a given mock key. Returns the helper object to allow
+     * chaining of calls.
+     *
+     * @param mockKey The mock key to set the mock for.
+     * @param mockDefinition The mock definition to set.
+     */
+    public setMock(
+        mockKey: string,
+        mockDefinition: unknown,
+    ): MockImportHelper<T> {
+        if (typeof mockKey !== 'string' || mockKey.length <= 0) {
+            throw new Error('Invalid mockKey (arg #1)');
+        }
+
+        if (!this._mockDeclarations[mockKey]) {
+            throw new Error(
+                `Mock key [${mockKey}] was not declared for import`,
+            );
+        }
+        this._mockDefinitions[mockKey] = mockDefinition;
+        return this;
+    }
+
+    /**
+     * Clears the mock for a given mock key. Returns the helper object to allow
+     * chaining of calls.
+     *
+     * @param mockKey The mock key to clear the mock for.
+     */
+    public clearMock(mockKey: string): MockImportHelper<T> {
+        if (typeof mockKey !== 'string' || mockKey.length <= 0) {
+            throw new Error('Invalid mockKey (arg #1)');
+        }
+
+        if (!this._mockDeclarations[mockKey]) {
+            throw new Error(
+                `Mock key [${mockKey}] was not declared for import`,
+            );
+        }
+        delete this._mockDefinitions[mockKey];
+        return this;
+    }
+
+    /**
+     * Gets mock definitions for libraries and local modules. These definitions
+     * will only include mocks that were set using the `setMock` method.
+     */
+    public getLibs(): MockDefinitions {
+        return this._getMockDefinitions()
+            .filter((mockInfo) => !mockInfo.isGlobal)
+            .reduce(
+                (result: Record<string, any>, mockInfo: MockDefinitionMap) => {
+                    result[mockInfo.importPath] = mockInfo.mockDefinition;
+                    return result;
+                },
+                {},
+            );
+    }
+
+    /**
+     * Gets mock definitions for global imports. These definitions will only
+     * include mocks that were set using the `setMock` method.
+     */
+    public getGlobals(): MockDefinitions {
+        return this._getMockDefinitions()
+            .filter((mockInfo) => mockInfo.isGlobal)
+            .reduce(
+                (result: Record<string, any>, mockInfo: MockDefinitionMap) => {
+                    result[mockInfo.importPath] = mockInfo.mockDefinition;
+                    return result;
+                },
+                {},
+            );
+    }
+}
---
+++ test/unit/mock-import-helper-spec.ts
@@ -0,0 +1,363 @@
+import { expect, use as _useWithChai } from 'chai';
+import _chaiAsPromised from 'chai-as-promised';
+import _sinonChai from 'sinon-chai';
+import _path from 'path';
+import 'mocha';
+
+_useWithChai(_sinonChai);
+_useWithChai(_chaiAsPromised);
+
+import {
+    MockDeclarations,
+    MockImportHelper,
+} from '../../src/mock-import-helper.js';
+
+describe('MockImportHelper', function () {
+    class Mockable {}
+
+    function _createInstance(
+        importPath: string = 'path/to/module',
+        mockDeclarations: MockDeclarations = {
+            foo: 'foolib',
+            bar: 'project://src/bar',
+            baz: 'global://console',
+        },
+        srcRoot: string = './src',
+    ): MockImportHelper<Mockable> {
+        return new MockImportHelper<Mockable>(
+            importPath,
+            mockDeclarations,
+            srcRoot,
+        );
+    }
+
+    describe('ctor()', function () {
+        [undefined, null, 123, true, [], '', {}, () => undefined].forEach(
+            (value) => {
+                it(`should throw an error if invoked without a valid import path (value=${value})`, async function () {
+                    const error = 'Invalid importPath (arg #1)';
+
+                    /* eslint-disable tsel/no-explicit-any */
+                    const importPath = value as any;
+                    const mockDeclarations = {};
+                    const srcRoot = './src';
+
+                    const wrapper = () =>
+                        new MockImportHelper<Mockable>(
+                            importPath,
+                            mockDeclarations,
+                            srcRoot,
+                        );
+
+                    expect(wrapper).to.throw(error);
+                });
+            },
+        );
+
+        [undefined, null, 123, true, [], 'foo', () => undefined].forEach(
+            (value) => {
+                it(`should throw an error if invoked without valid mock declarations (value=${value})`, async function () {
+                    const error = 'Invalid mockDeclarations (arg #2)';
+
+                    /* eslint-disable tsel/no-explicit-any */
+                    const importPath = 'path/to/module';
+                    const mockDeclarations = value as any;
+                    const srcRoot = './src';
+
+                    const wrapper = () =>
+                        new MockImportHelper<Mockable>(
+                            importPath,
+                            mockDeclarations,
+                            srcRoot,
+                        );
+
+                    expect(wrapper).to.throw(error);
+                });
+            },
+        );
+
+        [null, 123, true, [], {}, '', () => undefined].forEach((value) => {
+            it(`should throw an error if invoked with an invalid srcRoot (value=${value})`, async function () {
+                const error = 'Invalid srcRoot (arg #3)';
+
+                /* eslint-disable tsel/no-explicit-any */
+                const importPath = 'path/to/module';
+                const mockDeclarations = {};
+                const srcRoot = value as any;
+
+                const wrapper = () =>
+                    new MockImportHelper<Mockable>(
+                        importPath,
+                        mockDeclarations,
+                        srcRoot,
+                    );
+
+                expect(wrapper).to.throw(error);
+            });
+        });
+    });
+
+    describe('[props]', function () {
+        describe('importPath', function () {
+            [
+                {
+                    srcRoot: 'src',
+                    importPath: 'project://path/to/module',
+                    expected: _path.resolve('src/path/to/module'),
+                },
+                {
+                    srcRoot: 'src',
+                    importPath: './path/to/module',
+                    expected: _path.resolve('src/path/to/module'),
+                },
+                {
+                    srcRoot: 'src',
+                    importPath: '../path/to/module',
+                    expected: _path.resolve('path/to/module'),
+                },
+                {
+                    srcRoot: 'src',
+                    importPath: '/path/to/module',
+                    expected: '/path/to/module',
+                },
+                {
+                    srcRoot: 'src',
+                    importPath: 'node-lib',
+                    expected: 'node-lib',
+                },
+                {
+                    srcRoot: 'src',
+                    importPath: 'global://some-global-lib',
+                    expected: 'some-global-lib',
+                },
+            ].forEach(({ importPath, srcRoot, expected }) => {
+                it(`should return the module import path (srcRoot=${srcRoot}, importPath=${importPath})`, async function () {
+                    const mockDeclarations = {};
+
+                    const instance = _createInstance(
+                        importPath,
+                        mockDeclarations,
+                        srcRoot,
+                    );
+
+                    expect(instance.importPath).to.equal(expected);
+                });
+            });
+        });
+    });
+
+    describe('setMock()', function () {
+        [undefined, null, 123, true, [], '', {}, () => undefined].forEach(
+            (value) => {
+                it(`should throw an error if invoked without a valid mock key (value=${value})`, async function () {
+                    const error = 'Invalid mockKey (arg #1)';
+
+                    /* eslint-disable tsel/no-explicit-any */
+                    const mockKey = value as any;
+                    const mock = {};
+                    const instance = _createInstance();
+
+                    const wrapper = () => instance.setMock(mockKey, mock);
+                    expect(wrapper).to.throw(error);
+                });
+            },
+        );
+
+        it('should throw an error if the mock key was not included in the declarations', async function () {
+            const mockKey = 'bad-lib';
+            const mock = {};
+            const error = `Mock key [${mockKey}] was not declared for import`;
+
+            const instance = _createInstance();
+
+            const wrapper = () => instance.setMock(mockKey, mock);
+            expect(wrapper).to.throw(error);
+        });
+
+        it('should register the mock if the mock key was valid', async function () {
+            const mockKey = 'foo';
+            const mock = {};
+            const instance = _createInstance();
+
+            instance.setMock(mockKey, mock);
+
+            /// WARNING: This is a private member access. Should be used for
+            //testing only.
+            expect(instance['_mockDefinitions'][mockKey]).to.equal(mock);
+        });
+
+        it('should return an instance of the helper if the mock key was valid', async function () {
+            const mockKey = 'foo';
+            const mock = {};
+            const instance = _createInstance();
+
+            const ret = instance.setMock(mockKey, mock);
+
+            expect(ret).to.equal(instance);
+        });
+    });
+
+    describe('clearMock()', function () {
+        [undefined, null, 123, true, [], '', {}, () => undefined].forEach(
+            (value) => {
+                it(`should throw an error if invoked without a valid mock key (value=${value})`, async function () {
+                    const error = 'Invalid mockKey (arg #1)';
+
+                    /* eslint-disable tsel/no-explicit-any */
+                    const mockKey = value as any;
+                    const instance = _createInstance();
+
+                    const wrapper = () => instance.clearMock(mockKey);
+                    expect(wrapper).to.throw(error);
+                });
+            },
+        );
+
+        it('should throw an error if the mock key was not included in the declarations', async function () {
+            const mockKey = 'bad-lib';
+            const error = `Mock key [${mockKey}] was not declared for import`;
+
+            const instance = _createInstance();
+
+            const wrapper = () => instance.clearMock(mockKey);
+            expect(wrapper).to.throw(error);
+        });
+
+        it('should clear the mock registration if the mock key was valid', async function () {
+            const mockKey = 'foo';
+            const mock = {};
+            const instance = _createInstance();
+
+            instance.setMock(mockKey, mock);
+
+            /// WARNING: This is a private member access. Should be used for
+            //testing only.
+            expect(instance['_mockDefinitions'][mockKey]).to.equal(mock);
+
+            instance.clearMock(mockKey);
+
+            /// WARNING: This is a private member access. Should be used for
+            //testing only.
+            expect(instance['_mockDefinitions'][mockKey]).to.be.undefined;
+        });
+
+        it('should return an instance of the helper if the mock key was valid', async function () {
+            const mockKey = 'foo';
+            const instance = _createInstance();
+
+            const ret = instance.clearMock(mockKey);
+
+            expect(ret).to.equal(instance);
+        });
+    });
+
+    describe('getLibs()', function () {
+        it('should return an empty object if no mocks were registered', async function () {
+            const srcRoot = 'src';
+            const declarations = {
+                foo: 'foolib',
+                bar: 'project://subdir/bar',
+                baz: 'global://console',
+            };
+            const instance = _createInstance(undefined, declarations, srcRoot);
+            const ret = instance.getLibs();
+
+            expect(ret).to.be.an('object').that.is.empty;
+        });
+
+        it('should return an empty object if only globals are registered', async function () {
+            const srcRoot = 'src';
+            const declarations = {
+                foo: 'foolib',
+                bar: 'project://subdir/bar',
+                baz: 'global://console',
+            };
+            const instance = _createInstance(undefined, declarations, srcRoot);
+            instance.setMock('baz', {});
+            const ret = instance.getLibs();
+
+            expect(ret).to.be.an('object').that.is.empty;
+        });
+
+        it('should return mock library definitions for each mock that was registered', async function () {
+            const srcRoot = 'src';
+            const declarations = {
+                foo: 'foolib',
+                bar: 'project://subdir/bar',
+                baz: 'global://console',
+            };
+            const instance = _createInstance(undefined, declarations, srcRoot);
+
+            const fooMock = {};
+            const barMock = {};
+            const bazMock = {};
+
+            instance
+                .setMock('foo', fooMock)
+                .setMock('bar', barMock)
+                .setMock('baz', bazMock);
+
+            const ret = instance.getLibs();
+
+            expect(ret).to.deep.equal({
+                foolib: fooMock,
+                [_path.resolve('src', 'subdir', 'bar')]: barMock,
+            });
+        });
+    });
+
+    describe('getGlobals()', function () {
+        it('should return an empty object if no mocks were registered', async function () {
+            const srcRoot = 'src';
+            const declarations = {
+                foo: 'foolib',
+                bar: 'project://subdir/bar',
+                baz: 'global://console',
+            };
+            const instance = _createInstance(undefined, declarations, srcRoot);
+            const ret = instance.getGlobals();
+
+            expect(ret).to.be.an('object').that.is.empty;
+        });
+
+        it('should return an empty object if only globals are registered', async function () {
+            const srcRoot = 'src';
+            const declarations = {
+                foo: 'foolib',
+                bar: 'project://subdir/bar',
+                baz: 'global://console',
+            };
+            const instance = _createInstance(undefined, declarations, srcRoot);
+            instance.setMock('foo', {});
+            instance.setMock('bar', {});
+            const ret = instance.getGlobals();
+
+            expect(ret).to.be.an('object').that.is.empty;
+        });
+
+        it('should return mock library definitions for each mock that was registered', async function () {
+            const srcRoot = 'src';
+            const declarations = {
+                foo: 'foolib',
+                bar: 'project://subdir/bar',
+                baz: 'global://console',
+            };
+            const instance = _createInstance(undefined, declarations, srcRoot);
+
+            const fooMock = {};
+            const barMock = {};
+            const bazMock = {};
+
+            instance
+                .setMock('foo', fooMock)
+                .setMock('bar', barMock)
+                .setMock('baz', bazMock);
+
+            const ret = instance.getGlobals();
+
+            expect(ret).to.deep.equal({
+                console: bazMock,
+            });
+        });
+    });
+});
